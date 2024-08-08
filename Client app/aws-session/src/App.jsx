9824df52fbde037ff199f99f73f8e065c5eaca1e
prabuddhika.m <prabuddhika.m@eyepax.com>
2024-08-08 10:45:40 +0530

    first commit

---
+++ Client app/aws-session/src/App.jsx
@@ -0,0 +1,15 @@
+function App() {
+  return (
+    <>
+      <h1
+        style={{
+          textAlign: "center",
+        }}
+      >
+        Hello World
+      </h1>
+    </>
+  );
+}
+
+export default App;
